962fc57ef6ff355367e410d6385b16b4486195fc
Nasef161995~ <mnasef905@gmail.com>
2023-10-20 10:45:45 +0300

    000

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,9 +14,11 @@ size_t i;
 size_t j;
 int temp;
 int flag;
-for (i = 0; i < size - 1; i++)
+/*outer loop (number of passes)*/
+for (i = 0; i < size; i++)
 {
 flag = 0;
+/*inner loop (comparison and swap)*/
 for (j = 0; j < size - 1 - i; j++)
 {
 if (array[j] > array[j + 1])
@@ -24,8 +26,8 @@ if (array[j] > array[j + 1])
 temp = array[j];
 array[j] = array[j + 1];
 array[j + 1] = temp;
+print_array(array, size);/*print array after swapping*/
 flag = 1;
-print_array(array, size);
 }
 }
 
