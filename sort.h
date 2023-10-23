6fcc4fd33051bae375966cdf1409d070c5780d1a
Nasef161995~ <mnasef905@gmail.com>
2023-10-23 20:13:51 +0300

    4444

--- sort.h
+++ sort.h
@@ -22,8 +22,14 @@ struct listint_s *next;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+void swap(int *x, int *y);
+int partition(int array[], int lower, int upper, size_t size);
+void quicksort_recusion(int array[], int lower, int upper, size_t size);
 
 #endif /* SORT_H */
